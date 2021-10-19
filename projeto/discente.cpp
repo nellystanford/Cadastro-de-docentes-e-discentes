e9a0e4beaed53b338bee8cbc378148b52fa5ec2d
Nelly Stanford <58884606+nellystanford@users.noreply.github.com>
2021-10-18 23:15:32 -0300

    .

---
+++ projeto/discente.cpp
@@ -0,0 +1,30 @@
+#include "discente.hpp"
+
+using namespace std;
+
+Discente::Discente() {
+
+}
+
+void Discente::leAtributos() {
+    Pessoa::leAtributos();
+
+    cout << "Digite o ano de entrada do Discente: " << endl;
+    cin >> anoEntrada;
+
+    cout << "Digite o CRA do Discente: " << endl;
+    cin >> CRA;
+}
+
+void Discente::exibePessoas() {
+    Pessoa::exibePessoas();
+
+    cout << "Ano de entrada do discente: " << anoEntrada << endl;
+    cout << "CRA do discente: "            << CRA        << endl;
+    cout << "----------------------------------------------------------------" << endl;
+
+}
+
+void Discente::exibeNomes() {
+    Pessoa::exibeNomes();
+}
