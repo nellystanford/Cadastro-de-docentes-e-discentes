83d43d03fb73888194785b3e08603d6a6a5271c5
Nelly Stanford <58884606+nellystanford@users.noreply.github.com>
2021-10-18 23:17:23 -0300

    .

---
+++ projeto/endereco.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class Endereco {
+    public:
+        Endereco();
+
+        std::string rua;
+        std::string bairro;
+        std::string CEP;
+};
