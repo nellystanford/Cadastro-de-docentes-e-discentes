b7d65237277208e101a85fa6c16960ebf7ac53ef
Nelly Stanford <58884606+nellystanford@users.noreply.github.com>
2021-11-15 11:34:40 -0300

    .

--- projeto/main.cpp
+++ projeto/main.cpp
@@ -1,4 +1,5 @@
 #include "includes.hpp"
+#include <fstream>
 
 using namespace std;
 
@@ -11,6 +12,9 @@ int main () {
     int numRemocao;
     int op = 1, op2 = 0;
     int countDoc = 0, countDisc = 0;
+
+    // objeto para acessar o arquivo
+    ofstream arq;
 
     Menu menu;
 
@@ -132,6 +136,62 @@ int main () {
                     }
 
                 break;
+            
+            //exportar cadastros para um arquivo
+            case 5:
+
+                system(CLEAR);
+
+                //abertura do arquivo paa escrita ao final dos dados já existentes
+                arq.open("arquivo.txt", ios::app);
+
+                //verificação de erro na abertura do arquivo
+                if (!arq.is_open()) {
+                    cout << "Erro ao abrir o arquivo" << endl;
+                    sleep(2);
+                    break;
+                }
+
+                arq << "DOCENTES: " << countDoc << endl;
+               /* for(int i=0; i<countDoc; i++) {
+                    doc[i].escrevePessoas(arq);
+                }*/
+
+                for(int i=0; i<countDoc; i++) {
+                    arq << "-----------------------------------------------" << endl;
+                    arq << "Nome: " << doc[i].getNome() << endl;
+                    arq << "Sexo: " << doc[i].getSexo() << endl;
+                    arq << "Endereco: " << doc[i].getEndereco().rua << ", " << 
+                    doc[i].getEndereco().bairro << ". " << doc[i].getEndereco().CEP << "." << endl;
+                    arq << "Idade: " << doc[i].getIdade() << endl;
+                    arq << "Cadeira ensinada: " << doc[i].getCadeiraEnsinada() << endl;
+                    arq << "-----------------------------------------------" << endl;
+                }
+
+                arq << endl;
+                arq << "DISCENTES: " << countDisc << endl;
+
+                for(int i=0; i<countDisc; i++) {
+                    arq << "-----------------------------------------------" << endl;
+                    arq << "Nome: " << disc[i].getNome() << endl;
+                    arq << "Sexo: " << disc[i].getSexo() << endl;
+                    arq << "Endereco: " << disc[i].getEndereco().rua << ", " << 
+                    doc[i].getEndereco().bairro << ". " << disc[i].getEndereco().CEP << "." << endl;
+                    arq << "Idade: " << disc[i].getIdade() << endl;
+                    arq << "Ano de Entrada: " << disc[i].getAnoEntrada() << endl;
+                    arq << "CRA: " << disc[i].getCRA() << endl;
+                    arq << "-----------------------------------------------" << endl;
+                }
+
+                arq.close();
+
+                cout << "Arquivo exportado com sucesso!" << endl;
+                cout << "Pressione enter para continuar..." << endl;
+                cin.ignore();
+                getchar();
+
+                break;
+
             default:
                 system(CLEAR);
                 cout << "Comando invalido, por favor selecione novamente" << endl;
