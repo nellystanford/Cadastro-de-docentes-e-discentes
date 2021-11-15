975b518effbb68e1f376118fa16fd6450892ae62
Nelly Stanford <58884606+nellystanford@users.noreply.github.com>
2021-11-15 11:36:06 -0300

    .

--- projeto/pessoa.hpp
+++ projeto/pessoa.hpp
@@ -16,4 +16,9 @@ class Pessoa {
         virtual void leAtributos() = 0;
         virtual void exibePessoas() = 0;
         virtual void exibeNomes() = 0;
+
+        std::string getNome();
+        std::string getSexo();
+        Endereco getEndereco();
+        int getIdade();
 };
